da528c2a9ce328d1b6acfab8c03a8209553c59cb
xifeng.yang <yxf05427@163.com>
2023-08-20 16:23:47 +0800

    update code

--- app/src/main/java/com/learnopengles/android/lesson1/LessonOneRenderer.java
+++ app/src/main/java/com/learnopengles/android/lesson1/LessonOneRenderer.java
@@ -17,162 +17,77 @@ import javax.microedition.khronos.opengles.GL10;
  * renderers -- the static class GLES20 is used instead.
  */
 public class LessonOneRenderer implements GLSurfaceView.Renderer {
-    /**
-     * Store the model matrix. This matrix is used to move models from object space (where each model can be thought
-     * of being located at the center of the universe) to world space.
-     */
     private float[] mModelMatrix = new float[16];
-
-    /**
-     * Store the view matrix. This can be thought of as our camera. This matrix transforms world space to eye space;
-     * it positions things relative to our eye.
-     */
     private float[] mViewMatrix = new float[16];
-
-    /**
-     * Store the projection matrix. This is used to project the scene onto a 2D viewport.
-     */
     private float[] mProjectionMatrix = new float[16];
-
-    /**
-     * Allocate storage for the final combined matrix. This will be passed into the shader program.
-     */
     private float[] mMVPMatrix = new float[16];
 
-    /**
-     * Store our model data in a float buffer.
-     */
     private final FloatBuffer mTriangle1Vertices;
     private final FloatBuffer mTriangle2Vertices;
     private final FloatBuffer mTriangle3Vertices;
 
-    /**
-     * This will be used to pass in the transformation matrix.
-     */
     private int mMVPMatrixHandle;
-
-    /**
-     * This will be used to pass in model position information.
-     */
     private int mPositionHandle;
-
-    /**
-     * This will be used to pass in model color information.
-     */
     private int mColorHandle;
 
-    /**
-     * How many bytes per float.
-     */
-    private final int mBytesPerFloat = 4;
+    private static final int BYTES_PER_FLOAT = 4;
+    private static final int STRIDE_BYTES = 7 * BYTES_PER_FLOAT;
+    private static final int POSITION_OFFSET = 0;
+    private static final int POSITION_DATA_SIZE = 3;
+    private static final int COLOR_OFFSET = 3;
+    private static final int COLOR_DATA_SIZE = 4;
 
-    /**
-     * How many elements per vertex.
-     */
-    private final int mStrideBytes = 7 * mBytesPerFloat;
-
-    /**
-     * Offset of the position data.
-     */
-    private final int mPositionOffset = 0;
-
-    /**
-     * Size of the position data in elements.
-     */
-    private final int mPositionDataSize = 3;
-
-    /**
-     * Offset of the color data.
-     */
-    private final int mColorOffset = 3;
-
-    /**
-     * Size of the color data in elements.
-     */
-    private final int mColorDataSize = 4;
-
-    /**
-     * Initialize the model data.
-     */
     public LessonOneRenderer() {
-        // Define points for equilateral triangles.
-
-        // This triangle is red, green, and blue.
         final float[] triangle1VerticesData = {
-                // X, Y, Z,
-                // R, G, B, A
-                -0.5f, -0.25f, 0.0f,
-                1.0f, 0.0f, 0.0f, 1.0f,
+                // X, Y, Z,R, G, B, A
+                -0.5f, -0.25f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+                0.5f, -0.25f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+                0.0f, 0.559016994f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f
+        };
 
-                0.5f, -0.25f, 0.0f,
-                0.0f, 0.0f, 1.0f, 1.0f,
-
-                0.0f, 0.559016994f, 0.0f,
-                0.0f, 1.0f, 0.0f, 1.0f};
-
-        // This triangle is yellow, cyan, and magenta.
         final float[] triangle2VerticesData = {
-                // X, Y, Z,
-                // R, G, B, A
-                -0.5f, -0.25f, 0.0f,
-                1.0f, 1.0f, 0.0f, 1.0f,
+                // X, Y, Z,R, G, B, A
+                -0.5f, -0.25f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+                0.5f, -0.25f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+                0.0f, 0.559016994f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f
+        };
 
-                0.5f, -0.25f, 0.0f,
-                0.0f, 1.0f, 1.0f, 1.0f,
-
-                0.0f, 0.559016994f, 0.0f,
-                1.0f, 0.0f, 1.0f, 1.0f};
-
-        // This triangle is white, gray, and black.
         final float[] triangle3VerticesData = {
-                // X, Y, Z,
-                // R, G, B, A
-                -0.5f, -0.25f, 0.0f,
-                1.0f, 1.0f, 1.0f, 1.0f,
+                // X, Y, Z, R, G, B, A
+                -0.5f, -0.25f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+                0.5f, -0.25f, 0.0f, 0.5f, 0.5f, 0.5f, 1.0f,
+                0.0f, 0.559016994f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f
+        };
 
-                0.5f, -0.25f, 0.0f,
-                0.5f, 0.5f, 0.5f, 1.0f,
+        mTriangle1Vertices = ByteBuffer.
+                allocateDirect(triangle1VerticesData.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer()
+                .put(triangle1VerticesData);
+        mTriangle1Vertices.position(0);
 
-                0.0f, 0.559016994f, 0.0f,
-                0.0f, 0.0f, 0.0f, 1.0f};
+        mTriangle2Vertices = ByteBuffer
+                .allocateDirect(triangle2VerticesData.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer()
+                .put(triangle2VerticesData);
+        mTriangle2Vertices.position(0);
 
-        // Initialize the buffers.
-        mTriangle1Vertices = ByteBuffer.allocateDirect(triangle1VerticesData.length * mBytesPerFloat)
-                .order(ByteOrder.nativeOrder()).asFloatBuffer();
-        mTriangle2Vertices = ByteBuffer.allocateDirect(triangle2VerticesData.length * mBytesPerFloat)
-                .order(ByteOrder.nativeOrder()).asFloatBuffer();
-        mTriangle3Vertices = ByteBuffer.allocateDirect(triangle3VerticesData.length * mBytesPerFloat)
-                .order(ByteOrder.nativeOrder()).asFloatBuffer();
-
-        mTriangle1Vertices.put(triangle1VerticesData).position(0);
-        mTriangle2Vertices.put(triangle2VerticesData).position(0);
-        mTriangle3Vertices.put(triangle3VerticesData).position(0);
+        mTriangle3Vertices = ByteBuffer
+                .allocateDirect(triangle3VerticesData.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer()
+                .put(triangle3VerticesData);
+        mTriangle3Vertices.position(0);
     }
 
     @Override
     public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
-        // Set the background clear color to gray.
         GLES20.glClearColor(0.5f, 0.5f, 0.5f, 0.5f);
-
-        // Position the eye behind the origin.
-        final float eyeX = 0.0f;
-        final float eyeY = 0.0f;
-        final float eyeZ = 1.5f;
-
-        // We are looking toward the distance
-        final float lookX = 0.0f;
-        final float lookY = 0.0f;
-        final float lookZ = -5.0f;
-
-        // Set our up vector. This is where our head would be pointing were we holding the camera.
-        final float upX = 0.0f;
-        final float upY = 1.0f;
-        final float upZ = 0.0f;
-
-        // Set the view matrix. This matrix can be said to represent the camera position.
-        // NOTE: In OpenGL 1, a ModelView matrix is used, which is a combination of a model and
-        // view matrix. In OpenGL 2, we can keep track of these matrices separately if we choose.
-        Matrix.setLookAtM(mViewMatrix, 0, eyeX, eyeY, eyeZ, lookX, lookY, lookZ, upX, upY, upZ);
+        Matrix.setLookAtM(mViewMatrix, 0,
+                0.0f, 0.0f, 1.5f, //相机位置
+                0.0f, 2.0f, 0f, //目标位置
+                0.0f, 1.0f, 0.0f); //相机正上方向量
 
         final String vertexShader =
                 "uniform mat4 u_MVPMatrix;      \n"        // A constant representing the combined model/view/projection matrix.
@@ -202,20 +117,13 @@ public class LessonOneRenderer implements GLSurfaceView.Renderer {
 
         // Load in the vertex shader.
         int vertexShaderHandle = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
-
         if (vertexShaderHandle != 0) {
-            // Pass in the shader source.
-            GLES20.glShaderSource(vertexShaderHandle, vertexShader);
-
-            // Compile the shader.
-            GLES20.glCompileShader(vertexShaderHandle);
-
-            // Get the compilation status.
-            final int[] compileStatus = new int[1];
+            GLES20.glShaderSource(vertexShaderHandle, vertexShader);// Pass in the shader source.
+            GLES20.glCompileShader(vertexShaderHandle); // Compile the shader.
+            final int[] compileStatus = new int[1]; // Get the compilation status.
             GLES20.glGetShaderiv(vertexShaderHandle, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
-
-            // If the compilation failed, delete the shader.
             if (compileStatus[0] == 0) {
+                // If the compilation failed, delete the shader.
                 GLES20.glDeleteShader(vertexShaderHandle);
                 vertexShaderHandle = 0;
             }
@@ -229,18 +137,13 @@ public class LessonOneRenderer implements GLSurfaceView.Renderer {
         int fragmentShaderHandle = GLES20.glCreateShader(GLES20.GL_FRAGMENT_SHADER);
 
         if (fragmentShaderHandle != 0) {
-            // Pass in the shader source.
-            GLES20.glShaderSource(fragmentShaderHandle, fragmentShader);
 
-            // Compile the shader.
-            GLES20.glCompileShader(fragmentShaderHandle);
-
-            // Get the compilation status.
-            final int[] compileStatus = new int[1];
+            GLES20.glShaderSource(fragmentShaderHandle, fragmentShader); // Pass in the shader source.
+            GLES20.glCompileShader(fragmentShaderHandle); // Compile the shader.
+            final int[] compileStatus = new int[1]; // Get the compilation status.
             GLES20.glGetShaderiv(fragmentShaderHandle, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
-
-            // If the compilation failed, delete the shader.
             if (compileStatus[0] == 0) {
+                // If the compilation failed, delete the shader.
                 GLES20.glDeleteShader(fragmentShaderHandle);
                 fragmentShaderHandle = 0;
             }
@@ -254,25 +157,15 @@ public class LessonOneRenderer implements GLSurfaceView.Renderer {
         int programHandle = GLES20.glCreateProgram();
 
         if (programHandle != 0) {
-            // Bind the vertex shader to the program.
-            GLES20.glAttachShader(programHandle, vertexShaderHandle);
-
-            // Bind the fragment shader to the program.
-            GLES20.glAttachShader(programHandle, fragmentShaderHandle);
-
-            // Bind attributes
-            GLES20.glBindAttribLocation(programHandle, 0, "a_Position");
+            GLES20.glAttachShader(programHandle, vertexShaderHandle); // Bind the vertex shader to the program.
+            GLES20.glAttachShader(programHandle, fragmentShaderHandle); // Bind the fragment shader to the program.
+            GLES20.glBindAttribLocation(programHandle, 0, "a_Position"); // Bind attributes
             GLES20.glBindAttribLocation(programHandle, 1, "a_Color");
-
-            // Link the two shaders together into a program.
-            GLES20.glLinkProgram(programHandle);
-
-            // Get the link status.
-            final int[] linkStatus = new int[1];
+            GLES20.glLinkProgram(programHandle); // Link the two shaders together into a program.
+            final int[] linkStatus = new int[1]; // Get the link status.
             GLES20.glGetProgramiv(programHandle, GLES20.GL_LINK_STATUS, linkStatus, 0);
-
-            // If the link failed, delete the program.
             if (linkStatus[0] == 0) {
+                // If the link failed, delete the program.
                 GLES20.glDeleteProgram(programHandle);
                 programHandle = 0;
             }
@@ -304,8 +197,7 @@ public class LessonOneRenderer implements GLSurfaceView.Renderer {
         final float bottom = -1.0f;
         final float top = 1.0f;
         final float near = 1.0f;
-        final float far = 10.0f;
-
+        final float far = 20.0f;
         Matrix.frustumM(mProjectionMatrix, 0, left, right, bottom, top, near, far);
     }
 
@@ -326,45 +218,31 @@ public class LessonOneRenderer implements GLSurfaceView.Renderer {
         Matrix.setIdentityM(mModelMatrix, 0);
         Matrix.translateM(mModelMatrix, 0, 0.0f, -1.0f, 0.0f);
         Matrix.rotateM(mModelMatrix, 0, 90.0f, 1.0f, 0.0f, 0.0f);
-        Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);
+        //Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);
         drawTriangle(mTriangle2Vertices);
 
         // Draw one translated a bit to the right and rotated to be facing to the left.
         Matrix.setIdentityM(mModelMatrix, 0);
-        Matrix.translateM(mModelMatrix, 0, 1.0f, 0.0f, 0.0f);
+        Matrix.translateM(mModelMatrix, 0, 0.5f, 0.0f, 0.0f);
         Matrix.rotateM(mModelMatrix, 0, 90.0f, 0.0f, 1.0f, 0.0f);
-        Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);
+        //Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);
         drawTriangle(mTriangle3Vertices);
     }
 
-    /**
-     * Draws a triangle from the given vertex data.
-     *
-     * @param aTriangleBuffer The buffer containing the vertex data.
-     */
     private void drawTriangle(final FloatBuffer aTriangleBuffer) {
-        // Pass in the position information
-        aTriangleBuffer.position(mPositionOffset);
-        GLES20.glVertexAttribPointer(mPositionHandle, mPositionDataSize, GLES20.GL_FLOAT, false,
-                mStrideBytes, aTriangleBuffer);
+        aTriangleBuffer.position(0);
+        GLES20.glVertexAttribPointer(mPositionHandle, POSITION_DATA_SIZE, GLES20.GL_FLOAT, false,
+                STRIDE_BYTES, aTriangleBuffer);
 
         GLES20.glEnableVertexAttribArray(mPositionHandle);
 
-        // Pass in the color information
-        aTriangleBuffer.position(mColorOffset);
-        GLES20.glVertexAttribPointer(mColorHandle, mColorDataSize, GLES20.GL_FLOAT, false,
-                mStrideBytes, aTriangleBuffer);
+        aTriangleBuffer.position(COLOR_OFFSET);
+        GLES20.glVertexAttribPointer(mColorHandle, COLOR_DATA_SIZE, GLES20.GL_FLOAT, false,
+                STRIDE_BYTES, aTriangleBuffer);
 
         GLES20.glEnableVertexAttribArray(mColorHandle);
-
-        // This multiplies the view matrix by the model matrix, and stores the result in the MVP matrix
-        // (which currently contains model * view).
         Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mModelMatrix, 0);
-
-        // This multiplies the modelview matrix by the projection matrix, and stores the result in the MVP matrix
-        // (which now contains model * view * projection).
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
-
         GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mMVPMatrix, 0);
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
     }
--- app/src/main/java/com/learnopengles/android/lesson2/LessonTwoActivity.java
+++ app/src/main/java/com/learnopengles/android/lesson2/LessonTwoActivity.java
@@ -7,54 +7,49 @@ import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
-public class LessonTwoActivity extends Activity 
-{
-	/** Hold a reference to our GLSurfaceView */
-	private GLSurfaceView mGLSurfaceView;
+public class LessonTwoActivity extends Activity {
+    /**
+     * Hold a reference to our GLSurfaceView
+     */
+    private GLSurfaceView mGLSurfaceView;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) 
-	{
-		super.onCreate(savedInstanceState);
-		
-		mGLSurfaceView = new GLSurfaceView(this);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		// Check if the system supports OpenGL ES 2.0.
-		final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-		final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
-		final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+        mGLSurfaceView = new GLSurfaceView(this);
 
-		if (supportsEs2) 
-		{
-			// Request an OpenGL ES 2.0 compatible context.
-			mGLSurfaceView.setEGLContextClientVersion(2);
+        // Check if the system supports OpenGL ES 2.0.
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
 
-			// Set the renderer to our demo renderer, defined below.
-			mGLSurfaceView.setRenderer(new LessonTwoRenderer());
-		} 
-		else 
-		{
-			// This is where you could create an OpenGL ES 1.x compatible
-			// renderer if you wanted to support both ES 1 and ES 2.
-			return;
-		}
+        if (supportsEs2) {
+            // Request an OpenGL ES 2.0 compatible context.
+            mGLSurfaceView.setEGLContextClientVersion(2);
 
-		setContentView(mGLSurfaceView);
-	}
+            // Set the renderer to our demo renderer, defined below.
+            mGLSurfaceView.setRenderer(new LessonTwoRenderer());
+        } else {
+            // This is where you could create an OpenGL ES 1.x compatible
+            // renderer if you wanted to support both ES 1 and ES 2.
+            return;
+        }
 
-	@Override
-	protected void onResume() 
-	{
-		// The activity must call the GL surface view's onResume() on activity onResume().
-		super.onResume();
-		mGLSurfaceView.onResume();
-	}
+        setContentView(mGLSurfaceView);
+    }
 
-	@Override
-	protected void onPause() 
-	{
-		// The activity must call the GL surface view's onPause() on activity onPause().
-		super.onPause();
-		mGLSurfaceView.onPause();
-	}	
+    @Override
+    protected void onResume() {
+        // The activity must call the GL surface view's onResume() on activity onResume().
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        // The activity must call the GL surface view's onPause() on activity onPause().
+        super.onPause();
+        mGLSurfaceView.onPause();
+    }
 }
